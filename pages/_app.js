73bf11acb4faf21734f6a15dc10ac26adae387ed
Mitchell Lemisa <139455432+stypsspace@users.noreply.github.com>
2023-08-22 17:11:41 +0200

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import Layout from '@/components/layout';
 import 'src/app/styles/globals.css';
+import 'src/app/styles/custom.css';
+import 'src/app/styles/responsive.css';
 // import 'typeface-inter';
 import Head from 'next/head'; // Import the Head component
 
