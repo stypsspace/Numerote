5e4a2897b2c0932024c55382903d77f162af028d
Mitchell Lemisa <139455432+stypsspace@users.noreply.github.com>
2023-08-22 17:20:26 +0200

    Update

---
+++ pages/posts/[slug].js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Post = () => {
+  return (
+    <div>
+      <h1>Placeholder Title</h1>
+      <p>This is a placeholder for the post content.</p>
+    </div>
+  );
+};
+
+export default Post;
